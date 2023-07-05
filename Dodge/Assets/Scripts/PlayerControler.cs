a06a0abfa07dbc5d1bc574f8c5dc7a7fe1ad14ef
99Leejunyoung <dlwnsdud0612@gmail.com>
2023-07-05 17:57:29 +0900

    [ljy] feat: 닷지게임 완성

--- Dodge/Assets/Scripts/PlayerControler.cs
+++ Dodge/Assets/Scripts/PlayerControler.cs
@@ -15,27 +15,7 @@ public class PlayerControler : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        //if(Input.GetKey(KeyCode.UpArrow) == true)
-        //{
-        //    playerRigid.AddForce(0f, 0f, speed);
-        //}
-
-        //if (Input.GetKey(KeyCode.DownArrow) == true)
-        //{
-        //    playerRigid.AddForce(0f, 0f, -speed);
-        //}
-
-        //if (Input.GetKey(KeyCode.RightArrow) == true)
-        //{
-        //    playerRigid.AddForce(speed, 0f,0f );
-        //}
-
-        //if (Input.GetKey(KeyCode.LeftArrow) == true)
-        //{
-        //    playerRigid.AddForce(-speed, 0f, 0f);
-        //}
-
+    {        
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
@@ -50,5 +30,8 @@ public class PlayerControler : MonoBehaviour
     public void Die()
     {
         gameObject.SetActive(false);
+
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.EndGame();
     }   
 }   // class PlayerControler
